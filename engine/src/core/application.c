dccd5858aa71ab5483d4c1ad7d4419038a6290cb
glomdom <22115@uktc-bg.com>
2024-07-02 01:26:10 +0300

    feat: windows input

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -5,6 +5,7 @@
 #include "platform/platform.h"
 #include "core/boobs_memory.h"
 #include "core/event.h"
+#include "core/input.h"
 
 typedef struct application_state {
     game* game_inst;
@@ -20,6 +21,9 @@ typedef struct application_state {
 static b8 initialized = FALSE;
 static application_state app_state;
 
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context);
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context);
+
 b8 application_create(game* game_inst) {
     if (initialized) {
         BOOBS_FATAL("application_create called more than once");
@@ -30,6 +34,7 @@ b8 application_create(game* game_inst) {
     app_state.game_inst = game_inst;
 
     initialize_logging();
+    input_initialize();
 
     app_state.is_running = TRUE;
     app_state.is_suspended = FALSE;
@@ -39,6 +44,10 @@ b8 application_create(game* game_inst) {
 
         return FALSE;
     }
+
+    event_register(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+    event_register(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+    event_register(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
 
     if (!platform_startup(
         &app_state.platform,
@@ -91,8 +100,46 @@ b8 application_run() {
 
     app_state.is_running = FALSE;
 
+    event_unregister(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+    event_unregister(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+    event_unregister(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
     event_shutdown();
+    input_shutdown();
+
     platform_shutdown(&app_state.platform);
 
     return TRUE;
+}
+
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context) {
+    switch (code) {
+        case EVENT_CODE_APPLICATION_QUIT: {
+            BOOBS_INFO("EVENT_CODE_APPLICATION_QUIT recieved");
+            app_state.is_running = FALSE;
+
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context) {
+    if (code == EVENT_CODE_KEY_PRESSED) {
+        u16 key_code = context.data.u16[0];
+        if (key_code == KEY_ESCAPE) {
+            event_context data = {};
+            event_fire(EVENT_CODE_APPLICATION_QUIT, 0, data);
+
+            return TRUE;
+        } else {
+            BOOBS_DEBUG("%c pressed", key_code);
+        }
+    } else if (code == EVENT_CODE_KEY_RELEASED) {
+        u16 key_code = context.data.u16[0];
+
+        BOOBS_DEBUG("%c released", key_code);
+    }
+
+    return FALSE;
 }
