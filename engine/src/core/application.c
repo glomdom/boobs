68f019bd8ffbbdbb9c4bc6f94acd1b1cc1abfbdc
glomdom <103685817+glomdom@users.noreply.github.com>
2024-07-01 22:14:00 +0300

    feat: app entry point

---
+++ engine/src/core/application.c
@@ -0,0 +1,94 @@
+#include "application.h"
+#include "game_types.h"
+#include "logger.h"
+
+#include "platform/platform.h"
+
+typedef struct application_state {
+    game* game_inst;
+
+    b8 is_running;
+    b8 is_suspended;
+    platform_state platform;
+    i16 width;
+    i16 height;
+    f64 last_time;
+} application_state;
+
+static b8 initialized = FALSE;
+static application_state app_state;
+
+b8 application_create(game* game_inst) {
+    if (initialized) {
+        BOOBS_FATAL("application_create called more than once");
+
+        return FALSE;
+    }
+
+    app_state.game_inst = game_inst;
+
+    initialize_logging();
+
+    BOOBS_FATAL("meowed fatally");
+    BOOBS_ERROR("meowed errorly??");
+    BOOBS_WARN("meowed warnly");
+    BOOBS_INFO("meowed informatically");
+    BOOBS_DEBUG("meowed debugically");
+    BOOBS_TRACE("meowed tracefully");
+
+    app_state.is_running = TRUE;
+    app_state.is_suspended = FALSE;
+
+    if (!platform_startup(
+        &app_state.platform,
+        game_inst->app_config.name,
+        game_inst->app_config.start_pos_x, game_inst->app_config.start_pos_y,
+        game_inst->app_config.start_width, game_inst->app_config.start_height)
+    ) {
+        return FALSE;
+    }
+
+    if (!app_state.game_inst->initialize(app_state.game_inst)) {
+        BOOBS_FATAL("game failed to initialize");
+
+        return FALSE;
+    }
+
+    app_state.game_inst->on_resize(app_state.game_inst, app_state.width, app_state.height);
+
+    initialized = TRUE;
+
+    return TRUE;
+}
+
+b8 application_run() {
+    while (app_state.is_running) {
+        if (!platform_pump_messages(&app_state.platform)) {
+            app_state.is_running = FALSE;
+        }
+
+        if (!app_state.is_suspended) {
+            if (!app_state.game_inst->update(app_state.game_inst, (f32)0)) {
+                BOOBS_FATAL("game update failed | shutting down");
+
+                app_state.is_running = FALSE;
+
+                break;
+            }
+
+            if (!app_state.game_inst->render(app_state.game_inst, (f32)0)) {
+                BOOBS_FATAL("game render failed | shutting down");
+
+                app_state.is_running = FALSE;
+
+                break;
+            }
+        }
+    }
+
+    app_state.is_running = FALSE;
+
+    platform_shutdown(&app_state.platform);
+
+    return TRUE;
+}
--- engine/src/platform/platform.h
+++ engine/src/platform/platform.h
@@ -6,12 +6,13 @@ typedef struct platform_state {
     void* internal_state;
 } platform_state;
 
-BOOBS_API b8 platform_startup(platform_state* plat_state, const char* application_name, i32 x, i32 y, i32 width, i32 height);
-BOOBS_API void platform_shutdown(platform_state* plat_state);
-BOOBS_API b8 platform_pump_messages(platform_state* plat_state);
+b8 platform_startup(platform_state* plat_state, const char* application_name, i32 x, i32 y, i32 width, i32 height);
+void platform_shutdown(platform_state* plat_state);
+b8 platform_pump_messages(platform_state* plat_state);
 
-void* platform_allocate(u64 size, b8 aligned);
-void platform_free(void* block, b8 aligned);
+// FIXME: Temporary expose until a memory subsystem has been implemented.
+BOOBS_API void* platform_allocate(u64 size, b8 aligned);
+BOOBS_API void platform_free(void* block, b8 aligned);
 void* platform_zero_copy(void* block, u64 size);
 void* platform_copy_memory(void* dest, const void* source, u64 size);
 void* platform_set_memory(void* dest, i32 value, u64 size);
