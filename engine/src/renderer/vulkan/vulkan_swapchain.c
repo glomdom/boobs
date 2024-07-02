00a4bd79022cb2c1933ce6f34efdb5f6fdaec147
glomdom <22115@uktc-bg.com>
2024-07-02 22:10:43 +0300

    chore: log destroy

--- engine/src/renderer/vulkan/vulkan_swapchain.c
+++ engine/src/renderer/vulkan/vulkan_swapchain.c
@@ -215,9 +215,15 @@ void create(vulkan_context* context, u32 width, u32 height, vulkan_swapchain* sw
 void destroy(vulkan_context* context, vulkan_swapchain* swapchain) {
     vulkan_image_destroy(context, &swapchain->depth_attachment);
 
+    BOOBS_INFO("destroyed depth image");
+
     for (u32 i = 0; i < swapchain->image_count; ++i) {
         vkDestroyImageView(context->device.logical_device, swapchain->views[i], context->allocator);
     }
 
+    BOOBS_INFO("destroyed swapchain images")
+
     vkDestroySwapchainKHR(context->device.logical_device, swapchain->handle, context->allocator);
+
+    BOOBS_INFO("destroyed swapchain")
 }
